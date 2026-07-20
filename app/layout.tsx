19e77486440d8f453dee3025f622a524064c2578
hussien tawhidi <hussientawhidi710@gmail.com>
2026-07-20 21:22:30 +0330

    update slid

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { iranYekan } from "@/lib/font";
 import Header from "@/components/header/Header";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import "./globals.css";
 import Footer from "@/components/footer/Footer";
 
